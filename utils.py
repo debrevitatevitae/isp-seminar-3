215b8b42e56c39fed54c4335b652befb6c7cc38b
debrevitatevitae <giotb92@gmail.com>
2025-12-11 12:24:49 +0100

    Debug MMD and reformat

--- utils.py
+++ utils.py
@@ -5,7 +5,6 @@ import numpy as np
 
 
 class BarsAndStripes:
-
     def __init__(self, n: int) -> None:
         """Dataset of binary-pixel images consisting of either bars or stripes.
         Can be used for unsupervised learning.
@@ -15,7 +14,7 @@ class BarsAndStripes:
         """
 
         self.n_pixels = n
-        self.size = n ** 2
+        self.size = n**2
 
         # build the dataset.
         bitstrings = [list(np.binary_repr(i, n))[::-1] for i in range(2**n)]
@@ -30,7 +29,9 @@ class BarsAndStripes:
         bars = np.repeat(bars, n, 1)
         bars = bars.reshape(2**n, n * n)
 
-        self.data = np.vstack((stripes[0 : stripes.shape[0] - 1], bars[1 : bars.shape[0]]))
+        self.data = np.vstack(
+            (stripes[0 : stripes.shape[0] - 1], bars[1 : bars.shape[0]])
+        )
 
         self.bitstrings = []
         self.int_labels = []
@@ -41,10 +42,8 @@ class BarsAndStripes:
         self.probs = np.zeros(2**self.size)
         self.probs[self.int_labels] = 1 / len(self.data)
 
-
     def sample_integer(self, n) -> np.ndarray:
         return np.random.choice(self.int_labels, size=n, p=self.probs[self.int_labels])
-
 
     def plot_sample(self, sample_id: int) -> matplotlib.figure.Figure:
         """Plots one of the bar/stripes images.
@@ -75,7 +74,6 @@ class BarsAndStripes:
                     fontsize=12,
                 )
 
-
     def plot_dataset(self) -> matplotlib.figure.Figure:
         """Plots the entire set of images."""
         n = self.n_pixels
@@ -88,7 +86,6 @@ class BarsAndStripes:
             plt.imshow(np.reshape(i, (n, n)), cmap="gray", vmin=0, vmax=1)
             plt.xticks([])
             plt.yticks([])
-
 
     def plot_data_dist(self) -> matplotlib.figure.Figure:
         """Plots the distribution of the bitstrings.
@@ -105,72 +102,95 @@ class BarsAndStripes:
         plt.subplots_adjust(bottom=0.3)
 
 
-class MMD_Gauss_Mix():
+class MMD_Gauss_Mix:
     def __init__(self, scales, circuit, target_dist, n_shots):
         self.scales = scales
-        self.gammas = 1 / (2 * scales ** 2)
+        self.gammas = 1 / (2 * scales)
         self.circuit = circuit
         self.target_dist = target_dist
         self.n_shots = n_shots
-        
+
         self.weights = None
 
     def get_circuit_samples(self, weights):
-        return jnp.array([jnp.dot(pred, 2 ** jnp.arange(pred.size -1, -1, -1)) for pred in self.circuit(weights)])
+        return jnp.array(
+            [
+                jnp.dot(pred, 2 ** jnp.arange(pred.size - 1, -1, -1))
+                for pred in self.circuit(weights)
+            ]
+        )
 
     def get_target_samples(self):
         return jnp.array(self.target_dist(self.n_shots))
 
     def compute_kernel(self, x, y):
-        k_xy = 0.
+        k_xy = 0.0
         for gamma in self.gammas:
-            k_xy += jnp.exp(gamma * (x-y)**2)
-            
+            k_xy += jnp.exp(-gamma * (x - y) ** 2)
+
         return k_xy / len(self.scales)
 
     def compute_kernel_expv(self, x_samples, y_samples):
-        exp_k = 0.
+        exp_k = 0.0
         for x_i, y_i in zip(x_samples, y_samples):
             exp_k += self.compute_kernel(x_i, y_i)
 
         return exp_k / self.n_shots
 
     def compute_loss(self, weights):
-        exp_k_p_p = self.compute_kernel_expv(self.get_circuit_samples(weights), self.get_circuit_samples(weights))
-        exp_k_p_pi = self.compute_kernel_expv(self.get_circuit_samples(weights), self.get_target_samples())
-        exp_k_pi_pi = self.compute_kernel_expv(self.get_target_samples(), self.get_target_samples())
+        exp_k_p_p = self.compute_kernel_expv(
+            self.get_circuit_samples(weights), self.get_circuit_samples(weights)
+        )
+        exp_k_p_pi = self.compute_kernel_expv(
+            self.get_circuit_samples(weights), self.get_target_samples()
+        )
+        exp_k_pi_pi = self.compute_kernel_expv(
+            self.get_target_samples(), self.get_target_samples()
+        )
 
         return exp_k_p_p - 2 * exp_k_p_pi + exp_k_pi_pi
 
     def compute_partial_weight(self, weights, index):
-        theta_plus = jnp.ravel(weights.copy())
-        theta_plus.at[index].add(jnp.pi/2)
-        theta_plus = jnp.reshape(theta_plus, shape=weights.shape)
-        
-        theta_minus = jnp.ravel(weights.copy())
-        theta_minus.at[index].subtract(jnp.pi/2)
-        theta_minus = jnp.reshape(theta_minus, shape=weights.shape)
-        
+        theta_plus = weights.copy()
+        theta_plus.at[index].add(jnp.pi / 2)
+
+        theta_minus = weights.copy()
+        theta_minus.at[index].subtract(jnp.pi / 2)
+
         # p_theta_+, p_theta
-        exp_k_p_plus_p = self.compute_kernel_expv(self.get_circuit_samples(theta_plus), self.get_circuit_samples(weights))
-        
+        exp_k_p_plus_p = self.compute_kernel_expv(
+            self.get_circuit_samples(theta_plus), self.get_circuit_samples(weights)
+        )
+
         # p_theta_-, p_theta
-        exp_k_p_minus_p = self.compute_kernel_expv(self.get_circuit_samples(theta_minus), self.get_circuit_samples(weights))
-        
+        exp_k_p_minus_p = self.compute_kernel_expv(
+            self.get_circuit_samples(theta_minus), self.get_circuit_samples(weights)
+        )
+
         # p_theta_+, pi
-        exp_k_p_plus_pi = self.compute_kernel_expv(self.get_circuit_samples(theta_plus), self.get_target_samples())
-        
+        exp_k_p_plus_pi = self.compute_kernel_expv(
+            self.get_circuit_samples(theta_plus), self.get_target_samples()
+        )
+
         # p_theta_-, pi
-        exp_k_p_minus_pi = self.compute_kernel_expv(self.get_circuit_samples(theta_minus), self.get_target_samples())
+        exp_k_p_minus_pi = self.compute_kernel_expv(
+            self.get_circuit_samples(theta_minus), self.get_target_samples()
+        )
 
         return exp_k_p_plus_p - exp_k_p_minus_p - exp_k_p_plus_pi + exp_k_p_minus_pi
 
     def compute_gradient(self, weights):
-        return jnp.array([self.compute_partial_weight(weights, i) for i in range(weights.size)])
+        grad = jnp.empty_like(weights)
+
+        for idx in np.ndindex(weights):
+            grad.at[idx].set(self.compute_partial_weight(weights, idx))
+
+        return grad
 
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
